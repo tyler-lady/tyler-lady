03d71ad14b29c568fd075cc7dcaf442dd066fbdd
@tyler-lady <tclady53@gmail.com>
2020-10-22 03:08:21 -0400

    Redesigned Layout, added Tabbed projects and proficiencies

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 /*TODO:
-Honestly, I do not know. 
+code script for project descriptions to display onHover over their images 
 */
+
+var logoDraft = document.getElementById("logoDraft");
+var myNav = document.getElementById("myNav");
+
+tester = () => {
+console.log(logoDraft.style);
+}
+tester();
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0){
+      logoDraft.style.display = "none";
+    }
+    else {
+        logoDraft.style.display = "inherit";
+    }
+  });
+
 
